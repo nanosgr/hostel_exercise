32a781f67f72cf400206fd4f93553bc797160148
nanosgr <sebastian.g.rios@gmail.com>
2025-02-02 20:16:12 -0300

    Primer commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Hostel Management",
+    'summary': "Manage Hostel easily",
+    'description': """
+        Efficiently manage the entire residential facility in the school.", # Supports reStructuredText(RST)
+        format (description is Deprecated),
+        """,
+    'author': "Sebastian Rios",
+    'sequence': -105,
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '18.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        "security/hostel_security.xml",
+        "security/ir.model.access.csv",
+        "views/hostel.xml",
+    ],
+}
